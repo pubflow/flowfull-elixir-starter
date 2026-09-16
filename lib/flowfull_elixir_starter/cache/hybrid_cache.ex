829f39b843fa9d9acbeabf726065fb83a11b93ab
Samuel Recio <samuelrecio@pubflow.com>
2026-09-16 12:23:28 -0500

    Better hybridcache

--- lib/flowfull_elixir_starter/cache/hybrid_cache.ex
+++ lib/flowfull_elixir_starter/cache/hybrid_cache.ex
@@ -1,28 +1,55 @@
 defmodule FlowfullElixirStarter.Cache.HybridCache do
-  @moduledoc false
+  @moduledoc """
+  Exclusive cache backend: Redis when the `:redis` process exists, otherwise Cachex.
+  Never dual-writes or backfills the other store.
+  """
 
   def get(key, db_fallback) when is_function(db_fallback, 0) do
-    case Cachex.get(:hybrid_cache, key) do
-      {:ok, nil} ->
-        case maybe_redis_get(key) do
-          nil ->
-            val = db_fallback.()
-            put(key, val)
-            val
+    if redis_enabled?() do
+      case maybe_redis_get(key) do
+        nil ->
+          val = db_fallback.()
+          maybe_redis_set(key, val)
+          val
 
-          v ->
-            v
-        end
+        v ->
+          v
+      end
+    else
+      case Cachex.get(:hybrid_cache, key) do
+        {:ok, nil} ->
+          val = db_fallback.()
+          Cachex.put(:hybrid_cache, key, val)
+          val
 
-      {:ok, v} ->
-        v
+        {:ok, v} ->
+          v
+      end
     end
   end
 
   def put(key, value) do
-    Cachex.put(:hybrid_cache, key, value)
-    maybe_redis_set(key, value)
+    if redis_enabled?() do
+      maybe_redis_set(key, value)
+    else
+      Cachex.put(:hybrid_cache, key, value)
+    end
+
     value
+  end
+
+  def delete(key) do
+    if redis_enabled?() do
+      _ = Redix.command(:redis, ["DEL", key])
+    else
+      Cachex.del(:hybrid_cache, key)
+    end
+
+    :ok
+  end
+
+  defp redis_enabled? do
+    Process.whereis(:redis) != nil
   end
 
   defp maybe_redis_get(key) do
